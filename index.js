8249e9fdb87f34cb0f986a42247db657a89ffd73
Wolfknightx <Wolfknightx@Gmail.com>
2018-02-13 10:56:23 -0600

    Letter constructor js page pulled together, now to work on the Word.js page.

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
  * Randomly selects a word and uses the Word constructor to store it
  * Prompts the user for each guess and keeps track of the user's remaining guesses*/
 var prompt = require("prompt");
+
+//pull the information from the cmd prompt based on what was pressed.
+var input = process.argv;
+var action = input[2];
+
+//the following variables allow access to the other js files.
+var words = require("./Words.js");
